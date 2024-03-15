5d2ba6df8e011eddb8deb7a7f7f97f41f7332d48
Marshal-Emanuel <marshcodes@gmail.com>
2024-03-15 03:16:41 +0300

    ussd1

--- index.php
+++ index.php
@@ -11,21 +11,64 @@ if ($text == "") {
     $response .= "1. Crime (Robbery/ Assault) \n";
     $response .= "2. Fire\n";
     $response .= "3. Medical emergency\n";
-    $response .= "4. Accident\n";
+    $response .= "4. Road Accident\n";
     $response .= "5. Domestic Violence\n";
     $response .= "6. Other\n";
 
 } else if ($text == "1") {
     // Business logic for first level response
-    $response = "CON Choose account information you want to view \n";
-    $response .= "1. Account number \n";
+    $response = "CON What is your location \n";
+    $response .= "1. Nairobi  \n";
+    $response .= "2. Chuka \n";
+    $response .= "3. Embu \n";
 
-} else if ($text == "2") {
-    // Business logic for first level response
-    // This is a terminal request. Note how we start the response with END
-    $response = "END Your phone number is ".$phoneNumber;
+} 
 
-} else if($text == "1*1") { 
+else if ($text == "2") {    
+  $response = "CON What is your location \n";
+  $response .= "1. Nairobi  \n";
+  $response .= "2. Chuka \n";
+  $response .= "3. Embu \n";
+
+}
+
+else if ($text == "3") {
+    
+  $response = "CON What is your location \n";
+  $response .= "1. Nairobi  \n";
+  $response .= "2. Chuka \n";
+  $response .= "3. Embu \n";
+
+}
+
+else if ($text == "4") {
+  
+  $response = "CON What is your location \n";
+  $response .= "1. Nairobi  \n";
+  $response .= "2. Chuka \n";
+  $response .= "3. Embu \n";
+
+}
+
+else if ($text == "5") {
+ 
+  $response = "CON What is your location \n";
+  $response .= "1. Nairobi  \n";
+  $response .= "2. Chuka \n";
+  $response .= "3. Embu \n";
+
+}
+
+else if ($text == "6") {
+  
+  $response = "CON What is your location \n";
+  $response .= "1. Nairobi  \n";
+  $response .= "2. Chuka \n";
+  $response .= "3. Embu \n";
+
+}
+
+else if($text == "1*1") { 
     // This is a second level response where the user selected 1 in the first instance
     $accountNumber  = "ACC1001";
 
