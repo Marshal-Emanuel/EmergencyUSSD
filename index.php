ba27ce748075cb9e106dfa3c2021bd00b5077c27
Marshal-Emanuel <marshcodes@gmail.com>
2024-03-28 02:55:31 +0300

    ussd update on functions

--- index.php
+++ index.php
@@ -5,76 +5,108 @@ $serviceCode = $_POST["serviceCode"];
 $phoneNumber = $_POST["phoneNumber"];
 $text        = $_POST["text"];
 
-if ($text == "") {
-    // This is the first request. Note how we start the response with CON
-    $response  = "CON ~ Emergency Report Sytem ~\n Select your emergency from below\n";
+//start the response with CON
+function mainMenu(){
+  $response  = "CON ~ Emergency Report System ~\n Select your emergency from below\n";
     $response .= "1. Crime (Robbery/ Assault) \n";
     $response .= "2. Fire\n";
     $response .= "3. Medical emergency\n";
     $response .= "4. Road Accident\n";
     $response .= "5. Domestic Violence\n";
     $response .= "6. Other\n";
+    return $response;
+}
+function location(){
+  $response = "CON What is your location \n";
+  $response .= "1. Nairobi  \n";
+  $response .= "2. Chuka \n";
+  $response .= "3. Embu \n";
+  return $response;
+}
+function nairobi(){
+  $response = "CON What is your sub-location \n";
+  $response .= "1. Lavington  \n";
+  $response .= "2. Upper Hill \n";
+  $response .= "3. Langata \n";
+  return $response;
+}
+function chuka(){
+  $response = "CON What is your sub-location \n";
+  $response .= "1. Chuka Town  \n";
+  $response .= "2. Chogoria \n";
+  $response .= "3. Ndagani \n";
+  return $response;
+}
 
-} else if ($text == "1") {
+function embu(){
+  $response = "CON What is your sublocation \n";
+  $response .= "1. Runyenjes  \n";
+  $response .= "2. Mbeeere South \n";
+  $response .= "3.c Manyatta \n";
+  return $response;
+}
+
+if ($text == "") {
+    $response = mainMenu();
+} 
+
+else if ($text == "1") {
     // Business logic for first level response
-    $response = "CON What is your location \n";
-    $response .= "1. Nairobi  \n";
-    $response .= "2. Chuka \n";
-    $response .= "3. Embu \n";
+    $report = "crime";
+    $response = location();
 
 } 
 
 else if ($text == "2") {    
-  $response = "CON What is your location \n";
-  $response .= "1. Nairobi  \n";
-  $response .= "2. Chuka \n";
-  $response .= "3. Embu \n";
-
+  $report = "fire";
+  $response = location();
 }
 
 else if ($text == "3") {
-    
-  $response = "CON What is your location \n";
-  $response .= "1. Nairobi  \n";
-  $response .= "2. Chuka \n";
-  $response .= "3. Embu \n";
+   $report = "Medical Emergency";
+   $response = location();
 
 }
 
 else if ($text == "4") {
   
-  $response = "CON What is your location \n";
-  $response .= "1. Nairobi  \n";
-  $response .= "2. Chuka \n";
-  $response .= "3. Embu \n";
+  $report = "Road Accident";
+  $response = location();
 
 }
 
 else if ($text == "5") {
- 
-  $response = "CON What is your location \n";
-  $response .= "1. Nairobi  \n";
-  $response .= "2. Chuka \n";
-  $response .= "3. Embu \n";
-
+  $report = "Domestic Violence";
+  $response = location();
 }
 
-else if ($text == "6") {
-  
-  $response = "CON What is your location \n";
-  $response .= "1. Nairobi  \n";
-  $response .= "2. Chuka \n";
-  $response .= "3. Embu \n";
+else if ($text == "6") {  
+  $report = "Other";
+  $response = location();
 
 }
 
 else if($text == "1*1") { 
-    // This is a second level response where the user selected 1 in the first instance
-    $accountNumber  = "ACC1001";
+  $location = "Nairobi";
+  $response = nairobi();
+  
+}
 
-    // This is a terminal request. Note how we start the response with END
-    $response = "END Your account number is ".$accountNumber;
-
+else if($text == "1*2") { 
+  $location = "Chuka";
+  $response = chuka();
+  
+}
+else if($text == "1*3") { 
+  $location = "Embu";
+  $response = embu();
+  
+}
+else if($text == "1*1*1"){
+  $sublocation = "Lavington";
+  //$response = "END Your report on ".$report."been recieved and fowarded to the Law enforcement Unit\n You will recieve a message with further guidelines";
+  $response = "CON Your report on has been recieved. ";
+  $response = "END Neccesary actions will be taken.";"
 }
 
 // Echo the response back to the API
